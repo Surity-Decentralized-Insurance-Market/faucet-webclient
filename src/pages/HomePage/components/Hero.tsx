9470169847273f04a29b42e567ec101680d3ec34
Spandan Barve <spandan567@gmail.com>
2024-05-23 14:31:52 +0530

    styling finishes

--- src/pages/HomePage/components/Hero.tsx
+++ src/pages/HomePage/components/Hero.tsx
@@ -2,6 +2,7 @@ import { ChangeEvent, useState } from "react";
 import useApiResponse from "../../../hooks/useApiResponse";
 import api from "../../../utils/api";
 import { isAddress } from "web3-validator";
+import { Link } from "react-router-dom";
 
 export default function Hero() {
   const config = useApiResponse(api.getConfiguration);
@@ -26,7 +27,7 @@ export default function Hero() {
   }
 
   return (
-    <section className="h-screen bg-slate-900 text-white flex flex-col items-center p-20 gap-y-10">
+    <section className="h-screen bg-slate-900 text-white flex flex-col items-center p-20 gap-y-10 relative">
       <div className="flex text-7xl items-center">
         <img src="/logo.png" className="h-[1.3em]" />
         <h1 className="font-semibold">Surity</h1>
@@ -85,6 +86,27 @@ export default function Hero() {
         </button>
         {claimable != true && claimable != false && <p>{claimable}</p>}
       </div>
+
+      <div className="bg-yellow-200 text-yellow-900 p-2 rounded-md text-sm">
+        You will receive these tokens on the{" "}
+        <Link
+          to="https://testfaucet.bt.io/#/"
+          target="_blank"
+          className="underline underline-offset-2 hover:no-underline duration-150"
+        >
+          BTTC Donau Testnet
+        </Link>
+      </div>
+
+      <div className="absolute bottom-6 left-1/2 -translate-x-1/2 text-center text-gray-300">
+        If these tokens are not needed any longer, make sure you send them back
+        to 0x54e4fb4CE1388e6A67D4d7Ffb231D5E46751e4cb
+      </div>
+      <img
+        src="/images/soparu.webp"
+        alt="soparu"
+        className="h-[30vh] absolute bottom-0 right-3"
+      />
     </section>
   );
 }
